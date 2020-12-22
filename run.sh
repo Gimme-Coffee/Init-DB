f83794462def535cd61fae76936c78ca1c32f3eb
Cassiel <soybeansoda@gmail.com>
2020-12-22 15:25:45 -0600

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Removing directories..."
+
+rm -fr sprites
+
+echo "Creating directories..."
+
+mkdir sprites
+
+mkdir sprites/front
+mkdir sprites/front-shiny
+
+mkdir sprites/back
+mkdir sprites/back-shiny
